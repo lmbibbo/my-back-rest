b51493de3c323e9ccc878e0ca93b78b7e93c955c
Luis Mariano Bibbo <lmbibbo@gmail.com>
2022-10-10 15:26:01 -0300

    Rest con Cors

--- src/main/java/com/luisma/mybackend/MyBackEndApplication.java
+++ src/main/java/com/luisma/mybackend/MyBackEndApplication.java
@@ -1,12 +1,16 @@
 package com.luisma.mybackend;
 
-import com.luisma.mybackend.enumeration.Status;
 import com.luisma.mybackend.model.Server;
 import com.luisma.mybackend.repository.ServerRepository;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static com.luisma.mybackend.enumeration.Status.SERVER_UP;
 
@@ -23,7 +27,7 @@ public class MyBackEndApplication {
 			serverRepository.save(new Server(null, "163.10.5.21", "Ubuntu", "16 Gb",
 					"Mi maq", "http://localhost:8080/server/image/server1.png", SERVER_UP));
 			serverRepository.save(new Server(null, "163.10.5.58", "Windows", "32 Gb",
-					"Otra maq", "http://localhost:8080/server/image/server2.png", SERVER_UP));
+					"Otras maq", "http://localhost:8080/server/image/server2.png", SERVER_UP));
 			serverRepository.save(new Server(null, "163.10.5.160", "Fedora", "8 Gb",
 					"Web Server", "http://localhost:8080/server/image/server3.png", SERVER_UP));
 			serverRepository.save(new Server(null, "163.10.5.14", "Red hat", "64 Gb",
@@ -31,5 +35,19 @@ public class MyBackEndApplication {
 		};
 	}
 
-
+	@Bean
+	public CorsFilter corsFilter(){
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Request-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Access-Control-Allow-Credentials", "Filename"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
 }
